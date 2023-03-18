9d383407857f606dfce1fc616a74732db3869760
Minsoo0405 <munsi0405@naver.com>
2023-03-18 23:38:13 +0900

    add Day01.java

---
+++ Day01/src/mycom/test/Practice03.java
@@ -0,0 +1,13 @@
+package mycom.test;
+
+public class Practice03 {
+    public static void main(String[] args) {
+        /* if ~ else 1 */
+        boolean b = true;
+        if(b == true){
+            System. out.println("변수 b는 true 입니다." );
+        } else{
+            System. out.println("변수 b는 false 입니다." );
+        }
+    }//main
+}
