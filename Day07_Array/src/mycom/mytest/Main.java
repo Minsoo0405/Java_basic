ea62c81e7929bbaeaaa31a0d5f755e3ad278f1a1
Minsoo0405 <munsi0405@naver.com>
2023-03-20 14:10:07 +0900

    add Day07_Array

---
+++ Day07_Array/src/mycom/mytest/Main.java
@@ -0,0 +1,14 @@
+package mycom.mytest;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] korScore = {30, 60, 90, 45, 75};
+        System.out.println(korScore[0]);
+
+        int[] korList = new int[4];
+        korList[0] = 88;
+        korList[1] = 89;
+        korList[2] = 45;
+        korList[3] = 99;
+    }
+};
